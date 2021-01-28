8e59108e5e16bc550684a6ffa57d8af180825415
Tobias Oho <tobias.oho@brainyoo.de>
2021-01-28 23:55:47 +0100

    GetRandomNumberTrivia UseCase

--- lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -1,17 +1,28 @@
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 import 'package:numbers_trivia_app/core/error/failures.dart';
+import 'package:numbers_trivia_app/core/usecases/usecase.dart';
 import 'package:numbers_trivia_app/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:numbers_trivia_app/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 
-class GetConcreteNumberTrivia {
+class GetConcreteNumberTrivia implements UseCase<NumberTrivia, Params> {
   final NumberTriviaRepository _repository;
 
   GetConcreteNumberTrivia(
     this._repository,
   );
 
-  Future<Either<Failure, NumberTrivia>> execute({@required int number}) async {
-    return await _repository.getConcreteNumberTrivia(number);
+  Future<Either<Failure, NumberTrivia>> call(Params params) async {
+    return await _repository.getConcreteNumberTrivia(params.number);
   }
+}
+
+class Params extends Equatable {
+  final int number;
+
+  Params({@required this.number});
+
+  @override
+  List<Object> get props => [number];
 }
---
+++ lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
@@ -0,0 +1,17 @@
+import 'package:dartz/dartz.dart';
+import 'package:numbers_trivia_app/core/error/failures.dart';
+import 'package:numbers_trivia_app/core/usecases/usecase.dart';
+import 'package:numbers_trivia_app/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:numbers_trivia_app/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+
+class GetRandomNumberTrivia implements UseCase<NumberTrivia, NoParams>{
+  final NumberTriviaRepository _repository;
+
+  GetRandomNumberTrivia(
+    this._repository,
+  );
+
+  Future<Either<Failure, NumberTrivia>> call(NoParams params) async {
+    return await _repository.getRadomNumberTrivia();
+  }
+}
